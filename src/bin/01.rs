747921baae8dca07a5b9b59ac91367a42d47f6d0
Adrian Colaianni <adrian@colaianni.us>
2025-12-14 12:58:35 -0500

    Completed day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,68 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut dial: u64 = 50;
+    let mut count = 0;
+    for line in input.lines() {
+        let left = line.get(..1).unwrap() == "L";
+        let mut amt: u64 = line.get(1..).unwrap().parse().unwrap();
+
+        if left {
+            if amt > 100 {
+                amt %= 100;
+            }
+            amt = 100 - amt;
+        }
+
+        dial += amt;
+        dial %= 100;
+
+        if dial == 0 {
+            count += 1;
+        }
+    }
+
+    Some(count)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut dial: i32 = 50;
+    let mut count = 0;
+    for line in input.lines() {
+        let left = line.get(..1).unwrap() == "L";
+        let amt: i32 = line.get(1..).unwrap().parse().unwrap();
+
+        if left {
+            dial -= amt;
+            while dial < 0 {
+                dial += 100;
+                count += 1;
+            }
+        } else {
+            dial += amt;
+            while dial > 99 {
+                dial -= 100;
+                count += 1;
+            }
+        }
+    }
+
+    Some(count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+}
