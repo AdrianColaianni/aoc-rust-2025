0ef06d0532bf78f05f5218cbe21b346119f20585
Adrian Colaianni <adrian@colaianni.us>
2025-12-16 23:27:54 -0500

    Completed day 4 part 1

---
+++ src/bin/04.rs
@@ -0,0 +1,115 @@
+advent_of_code::solution!(4);
+
+fn check_sides(input: &Vec<Vec<bool>>, p: (usize, usize), sides: &Vec<(isize, isize)>) -> bool {
+    if !input[p.0][p.1] {
+        return false;
+    }
+    let mut v = 0;
+    for s in sides {
+        let r = (p.0 as isize + s.0) as usize;
+        let c = (p.1 as isize + s.1) as usize;
+        if input[r][c] {
+            v += 1;
+        }
+    }
+    v < 4
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let sides: Vec<(isize, isize)> = vec![
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+    ];
+    let left_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.1 != -1)
+        .map(|i| i.to_owned())
+        .collect();
+    let right_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.1 != 1)
+        .map(|i| i.to_owned())
+        .collect();
+    let top_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.0 != -1)
+        .map(|i| i.to_owned())
+        .collect();
+    let bottom_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.0 != 1)
+        .map(|i| i.to_owned())
+        .collect();
+
+    let input: Vec<Vec<bool>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c == '@').collect())
+        .collect();
+
+    let mut sum = 0;
+    let rm = input.len() - 1;
+    let cm = input[0].len() - 1;
+
+    // Middle
+    for r in 1..rm {
+        for c in 1..cm {
+            if check_sides(&input, (r, c), &sides) {
+                sum += 1;
+            }
+        }
+    }
+
+    // Corners are always accessible
+    for (r, c) in [(0, 0), (0, cm), (rm, cm), (rm, 0)] {
+        if input[r][c] {
+            sum += 1;
+        }
+    }
+
+    // Edges
+    for r in 1..rm {
+        if check_sides(&input, (r, 0), &left_sides) {
+            sum += 1;
+        }
+        if check_sides(&input, (r, cm), &right_sides) {
+            sum += 1;
+        }
+    }
+    for c in 1..cm {
+        if check_sides(&input, (0, c), &top_sides) {
+            sum += 1;
+        }
+        if check_sides(&input, (rm, c), &bottom_sides) {
+            sum += 1;
+        }
+    }
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
