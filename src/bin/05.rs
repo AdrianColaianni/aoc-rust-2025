5db4a72c5978f2ceba44b7f6edf61018a6af2902
Adrian Colaianni <adrian@colaianni.us>
2025-12-17 02:42:57 -0500

    Completed day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 advent_of_code::solution!(5);
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -25,7 +27,27 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let range = input.split_once("\n\n").unwrap().0;
+    let mut ranges = BTreeSet::new();
+    for r in range.lines() {
+        let (s, e) = r.split_once('-').unwrap();
+        let s: usize = s.parse().unwrap();
+        let e: usize = e.parse().unwrap();
+        ranges.insert((s, e));
+    }
+
+    let (l, mut h) = ranges.pop_first().unwrap();
+    let ranges: Vec<_> = ranges.into_iter().collect();
+    let mut v = 0;
+    for (s, e) in ranges {
+        if h < s - 1 {
+            // Gap
+            v += s - h - 1;
+        }
+        h = h.max(e);
+    }
+
+    Some(h - l - v + 1)
 }
 
 #[cfg(test)]
@@ -41,6 +63,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 }
