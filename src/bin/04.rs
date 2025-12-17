a3390275d67b18f255a8bd6578dc0726d1f6df1f
Adrian Colaianni <adrian@colaianni.us>
2025-12-17 00:17:19 -0500

    Completed day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -93,8 +93,98 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(sum)
 }
 
+fn find(input: &Vec<Vec<bool>>) -> Vec<(usize, usize)> {
+    let mut res = Vec::new();
+    let sides: Vec<(isize, isize)> = vec![
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+    ];
+    let left_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.1 != -1)
+        .map(|i| i.to_owned())
+        .collect();
+    let right_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.1 != 1)
+        .map(|i| i.to_owned())
+        .collect();
+    let top_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.0 != -1)
+        .map(|i| i.to_owned())
+        .collect();
+    let bottom_sides: Vec<(isize, isize)> = sides
+        .iter()
+        .filter(|i| i.0 != 1)
+        .map(|i| i.to_owned())
+        .collect();
+
+    let rm = input.len() - 1;
+    let cm = input[0].len() - 1;
+
+    // Middle
+    for r in 1..rm {
+        for c in 1..cm {
+            if check_sides(&input, (r, c), &sides) {
+                res.push((r, c));
+            }
+        }
+    }
+
+    // Corners are always accessible
+    for (r, c) in [(0, 0), (0, cm), (rm, cm), (rm, 0)] {
+        if input[r][c] {
+            res.push((r, c));
+        }
+    }
+
+    // Edges
+    for r in 1..rm {
+        if check_sides(&input, (r, 0), &left_sides) {
+            res.push((r, 0));
+        }
+        if check_sides(&input, (r, cm), &right_sides) {
+            res.push((r, cm));
+        }
+    }
+    for c in 1..cm {
+        if check_sides(&input, (0, c), &top_sides) {
+            res.push((0, c));
+        }
+        if check_sides(&input, (rm, c), &bottom_sides) {
+            res.push((rm, c));
+        }
+    }
+
+    res
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut input: Vec<Vec<bool>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c == '@').collect())
+        .collect();
+
+    let mut t = 0;
+    loop {
+        let r = find(&input);
+        if r.is_empty() {
+            break;
+        }
+        t += r.len();
+        for r in r {
+            input[r.0][r.1] = false;
+        }
+    }
+
+    Some(t as u64)
 }
 
 #[cfg(test)]
@@ -110,6 +200,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
